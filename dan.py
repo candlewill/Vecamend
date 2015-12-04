4cc46361b7183dd6aad41cc2800fef1ee51de258
NLP <815333878@qq.com>
2015-12-04 23:46:28 +0800

    add dan model

--- dan.py
+++ dan.py
@@ -11,6 +11,7 @@ from keras.layers.core import Dense, Dropout, Activation, TimeDistributedMerge
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM, GRU
 from keras.datasets import imdb
+from sentiment_classification import build_keras_input
 
 '''
     Train a deep averaging network (DAN) using keras.
@@ -32,7 +33,12 @@ from keras.datasets import imdb
 
 
 
-def dan_original():
+def dan_original(max_features):
+    '''
+    DAN model
+    :param max_features: the number of words
+    :return: keras model
+    '''
     print('Build model...')
     model = Sequential()
     model.add(Embedding(max_features, 300))
@@ -46,8 +52,26 @@ def dan_original():
     model.add(Dense(input_dim=300, output_dim=1, activation = 'sigmoid'))
     return model
 
+def dan_pre_trained(max_features, weights=None):
+    '''
+    DAN model with pre-trained embeddings
+    :param max_features: the number of words
+    :return: keras model
+    '''
+    print('Build model...')
+    model = Sequential()
+    model.add(Embedding(input_dim = max_features, output_dim = 300, weights=[weights]))
+    model.add(TimeDistributedMerge(mode='ave'))
+    model.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(input_dim=300, output_dim=1, activation = 'sigmoid'))
+    return model
 
-if __name__ == "__main__":
+def test_dan_original():
     max_features = 20000
     maxlen = 100  # cut texts after this number of words (among top max_features most common words)
     batch_size = 32
@@ -57,13 +81,14 @@ if __name__ == "__main__":
     print(len(X_train), 'train sequences')
     print(len(X_test), 'test sequences')
 
+
     print("Pad sequences (samples x time)")
     X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
     X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
     print('X_train shape:', X_train.shape)
     print('X_test shape:', X_test.shape)
 
-    model = dan_original()
+    model = dan_original(max_features)
 
     # try using different optimizers and different optimizer configs
     model.compile(loss='binary_crossentropy', optimizer='adagrad', class_mode="binary")
@@ -73,3 +98,45 @@ if __name__ == "__main__":
     score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
     print('Test score:', score)
     print('Test accuracy:', acc)
+
+def SA_sst():
+    ((x_train_idx_data, y_train_valence, y_train_labels,
+     x_test_idx_data, y_test_valence, y_test_labels,
+     x_valid_idx_data, y_valid_valence, y_valid_labels,
+     x_train_polarity_idx_data, y_train_polarity,
+     x_test_polarity_idx_data, y_test_polarity,
+     x_valid_polarity_idx_data, y_valid_polarity), W) = build_keras_input()
+
+
+    maxlen = 100  # cut texts after this number of words (among top max_features most common words)
+    batch_size = 32
+    (X_train, y_train), (X_test, y_test), (X_valid, y_valide) = (x_train_polarity_idx_data, y_train_polarity), (x_test_polarity_idx_data, y_test_polarity), (x_valid_polarity_idx_data, y_valid_polarity)
+    print(len(X_train), 'train sequences')
+    print(len(X_test), 'test sequences')
+    # m= 0
+    # for i in X_train:
+    #     if len(i) >0:
+    #         for j in i:
+    #             if j > m:
+    #                 m=j
+    # print(m)
+    max_features = W.shape[0] # shape of W: (13631, 300)
+
+    print("Pad sequences (samples x time)")
+    X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+    X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+    print('X_train shape:', X_train.shape)
+    print('X_test shape:', X_test.shape)
+
+    model = dan_pre_trained(max_features, W)
+
+    # try using different optimizers and different optimizer configs
+    model.compile(loss='binary_crossentropy', optimizer='adagrad', class_mode="binary")
+
+    print("Train...")
+    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=18, validation_data=(X_test, y_test), show_accuracy=True)
+    score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
+    print('Test score:', score)
+    print('Test accuracy:', acc)
+if __name__ == "__main__":
+    SA_sst()
