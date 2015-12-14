c8c2ab9643daa7da0c1daa3db4870a52a1872adc
NLP <815333878@qq.com>
2015-12-14 18:52:04 +0800

    add model gird_search result

--- grid_search.py
+++ grid_search.py
@@ -1,12 +1,29 @@
 __author__ = 'NLP'
 from sklearn.grid_search import ParameterGrid
+import numpy as np
+from save_data import dump_picle
+
+
 def my_function(param1, param2):
-    pass
+    return param1+param2
 
 if __name__=='__main__':
-    param_grid = {'a': [1, 2], 'b': [True, False]}
+    param_grid = {'a': [1, 2], 'b': [4, 2]}
+    param_fitness = []
 
     grid = ParameterGrid(param_grid)
 
     for params in grid:
-        my_function(params['param1'], params['param2'])
+        print('calculating... parameter: %s' % str(params))
+        score = my_function(params['a'], params['b'])
+        print('Score: %s' % score)
+        param_fitness.append(score)
+
+    print('grid search complete.')
+    # return the best fitness value and its settings
+    best_fitness = np.min(np.array(param_fitness))
+    best_ind = np.where(np.array(param_fitness)==best_fitness)[0]
+    print('best fitness: %s' % best_fitness)
+    print('best setting: %s' % str(list(grid)[best_ind]))
+
+    dump_picle((param_grid, param_fitness), './tmp/grid_search_result.p')
