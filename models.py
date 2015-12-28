23ea9df7832c4154ec44a0300079750e48fb5c03
NLP <815333878@qq.com>
2015-12-29 01:00:24 +0800

    add examples

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import numpy as np
+
 np.random.seed(1337)  # for reproducibility
 
 from keras.preprocessing import sequence
@@ -45,29 +46,30 @@ def dan(weights=None):
     :param max_features: the number of words
     :return: keras model
     '''
-    max_features = weights.shape[0]      # weights.shape = (vocabulary size, vector dimension)
+    max_features = weights.shape[0]  # weights.shape = (vocabulary size, vector dimension)
     print('Build model...')
     model = Sequential()
-    model.add(Embedding(input_dim = max_features, output_dim = 300, weights=[weights], W_regularizer=l2(1e-5)))
+    model.add(Embedding(input_dim=max_features, output_dim=300, weights=[weights], W_regularizer=l2(1e-5)))
     model.add(Dropout(.5))
     model.add(TimeDistributedMerge(mode='ave'))
-    model.add(Dense(input_dim=300, output_dim=300, activation = 'relu', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
+    model.add(Dense(input_dim=300, output_dim=300, activation='relu', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
     model.add(Dropout(.4))
-    model.add(Dense(input_dim=300, output_dim=300, activation = 'relu', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
+    model.add(Dense(input_dim=300, output_dim=300, activation='relu', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
     model.add(Dropout(.2))
     # model.add(Dense(input_dim=300, output_dim=300, activation = 'relu', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
     # model.add(Dropout(.2))
-    model.add(Dense(input_dim=300, output_dim=2, activation = 'softmax', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
+    model.add(Dense(input_dim=300, output_dim=2, activation='softmax', W_regularizer=l2(1e-5), b_regularizer=l2(1e-5)))
     return model
+
 
 def cnn(W=None):
     # Number of feature maps (outputs of convolutional layer)
     N_fm = 20
     dense_nb = 20
     # kernel size of convolutional layer
-    kernel_size = 3
-    conv_input_width = W.shape[1]
-    conv_input_height = 200     # maxlen of sentence
+    kernel_size = 5
+    conv_input_width = W.shape[1]   # dims=300
+    conv_input_height = 200  # maxlen of sentence
 
     model = Sequential()
     # Embedding layer (lookup table of trainable word vectors)
@@ -76,28 +78,31 @@ def cnn(W=None):
     model.add(Reshape(dims=(1, conv_input_height, conv_input_width)))
 
     # first convolutional layer
-    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid', W_regularizer=l2(0.0001), activation = 'relu'))
+    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid',
+                            W_regularizer=l2(0.0001), activation='relu'))
     # ReLU activation
     model.add(Dropout(0.5))
 
     # aggregate data in every feature map to scalar using MAX operation
-    model.add(MaxPooling2D(pool_size=(conv_input_height-kernel_size+1, 1), border_mode='valid'))
-    model.add(Dropout(0.5))
+    # model.add(MaxPooling2D(pool_size=(conv_input_height-kernel_size+1, 1), border_mode='valid'))
+    model.add(MaxPooling2D(pool_size=(kernel_size * 5, 1), border_mode='valid'))
+    model.add(Dropout(0.4))
     model.add(Flatten())
-    model.add(Dense(output_dim=dense_nb, activation = 'relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(output_dim=dense_nb, activation='relu'))
+    model.add(Dropout(0.2))
     # Inner Product layer (as in regular neural network, but without non-linear activation function)
-    model.add(Dense(output_dim=2, activation = 'softmax'))
+    model.add(Dense(output_dim=2, activation='softmax'))
     # SoftMax activation; actually, Dense+SoftMax works as Multinomial Logistic Regression
     return model
+
 
 def imdb_cnn(W=None):
     # Number of feature maps (outputs of convolutional layer)
     N_fm = 20
     # kernel size of convolutional layer
     kernel_size = 3
-    dims = 300   # 300 dimension
-    maxlen = 200     # maxlen of sentence
+    dims = 300  # 300 dimension
+    maxlen = 200  # maxlen of sentence
     max_features = 5000
     hidden_dims = 10
     print('Build model...')
@@ -133,24 +138,26 @@ def imdb_cnn(W=None):
     model.add(Activation('softmax'))
     return model
 
+
 def Deep_CNN(W):
     # Number of feature maps (outputs of convolutional layer)
     N_fm = 20
     # kernel size of convolutional layer
     kernel_size = 3
-    conv_input_width = W.shape[1]   # 300 dimension
-    conv_input_height = 200     # maxlen of sentence
+    conv_input_width = W.shape[1]  # 300 dimension
+    conv_input_height = 200  # maxlen of sentence
 
     # Two Convolutional Layers with Pooling Layer
-    model = Sequential ()
+    model = Sequential()
     model.add(Embedding(input_dim=W.shape[0], output_dim=W.shape[1], weights=[W], W_constraint=unitnorm()))
     # Reshape word vectors from Embedding to tensor format suitable for Convolutional layer
     model.add(Reshape(dims=(1, conv_input_height, conv_input_width)))
-    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid', activation='relu'))
+    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid',
+                            activation='relu'))
     model.add(Dropout(0.4))
-    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size,nb_col=1, border_mode='valid',activation='relu'))
+    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=1, border_mode='valid', activation='relu'))
     model.add(Dropout(0.3))
-    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size,nb_col=1, border_mode='valid',activation='relu'))
+    model.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=1, border_mode='valid', activation='relu'))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(output_dim=N_fm, activation='relu'))
@@ -158,6 +165,7 @@ def Deep_CNN(W):
     # Fully Connected Layer as output layer
     model.add(Dense(output_dim=2, activation='softmax'))
     return model
+
 
 def hybrid_model(W):
     '''
@@ -170,31 +178,32 @@ def hybrid_model(W):
     # kernel size of convolutional layer
     kernel_size = 8
     conv_input_width = W.shape[1]
-    conv_input_height = 200     # maxlen of sentence
+    conv_input_height = 200  # maxlen of sentence
 
     cnn = Sequential()
-    cnn.add(Embedding(input_dim = max_features, output_dim = 300, weights=[W]))
+    cnn.add(Embedding(input_dim=max_features, output_dim=300, weights=[W]))
     cnn.add(Dropout(.5))
     cnn.add(Reshape(dims=(1, conv_input_height, conv_input_width)))
     # first convolutional layer
-    cnn.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid', W_regularizer=l2(0.0001), activation = 'relu'))
+    cnn.add(Convolution2D(nb_filter=N_fm, nb_row=kernel_size, nb_col=conv_input_width, border_mode='valid',
+                          W_regularizer=l2(0.0001), activation='relu'))
     # ReLU activation
     cnn.add(Dropout(0.5))
     # aggregate data in every feature map to scalar using MAX operation
-    cnn.add(MaxPooling2D(pool_size=(conv_input_height-kernel_size+1, 1), border_mode='valid'))
+    cnn.add(MaxPooling2D(pool_size=(conv_input_height - kernel_size + 1, 1), border_mode='valid'))
     cnn.add(Dropout(0.5))
     cnn.add(Flatten())
-    cnn.add(Dense(output_dim=N_fm, activation = 'relu'))
+    cnn.add(Dense(output_dim=N_fm, activation='relu'))
 
-    dan=Sequential()
-    dan.add(Embedding(input_dim = max_features, output_dim = 300, weights=[W]))
+    dan = Sequential()
+    dan.add(Embedding(input_dim=max_features, output_dim=300, weights=[W]))
     dan.add(Dropout(.5))
     dan.add(TimeDistributedMerge(mode='ave'))
-    dan.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    dan.add(Dense(input_dim=300, output_dim=300, activation='relu'))
     dan.add(Dropout(.5))
-    dan.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    dan.add(Dense(input_dim=300, output_dim=300, activation='relu'))
     dan.add(Dropout(.5))
-    dan.add(Dense(input_dim=300, output_dim=300, activation = 'relu'))
+    dan.add(Dense(input_dim=300, output_dim=300, activation='relu'))
 
     model = Sequential()
     model.add(Merge([cnn, dan], mode='sum'))
@@ -207,6 +216,7 @@ def hybrid_model(W):
     # model.fit([X_train,X_train], y_train, batch_size=batch_size, nb_epoch=100, validation_data=([X_test,X_test], y_test), show_accuracy=True, callbacks=[early_stopping])
     # score, acc = model.evaluate([X_test,X_test], y_test, batch_size=batch_size, show_accuracy=True)
 
+
 # def grid_search()
 
 
@@ -214,18 +224,49 @@ def hybrid_model(W):
 def ngrams_cnn(W):
     pass
 
+
+def parallel_cnn(W):
+    (nb_vocab, dims) =W.shape
+    N_filter=20
+
+    filter_shapes = [[2, 300], [3, 300], [4, 300], [5, 300]]
+    pool_shapes = [[25, 1], [24, 1], [23, 1], [22, 1]]  # Four Parallel Convolutional Layers with Four Pooling Layers
+    model = Sequential()
+    model.add(Embedding(input_dim=W.shape[0], output_dim=W.shape[1], weights=[W], W_constraint=unitnorm()))
+    # Reshape word vectors from Embedding to tensor format suitable for Convolutional layer
+    model.add(Reshape(dims=(1, 200, dims)))
+    sub_models = []
+    for i in range(len(pool_shapes)):
+        pool_shape = pool_shapes[i]
+        filter_shape = filter_shapes[i]
+        sub_model = Sequential()
+        sub_model.add(Convolution2D(input_shape=(1, 200, dims),nb_filter=N_filter, nb_row=filter_shape[0], nb_col=filter_shape[1], border_mode = 'valid', activation = 'relu'))
+        sub_model.add(MaxPooling2D(pool_size=(pool_shape[0], pool_shape[1]), border_mode='valid'))
+        sub_model.add(Flatten())
+        sub_models.append(sub_model)
+
+    model.add((Merge(sub_models, mode='concat')))
+    # Fully Connected Layer with dropout
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    # Fully Connected Layer as output layer
+    model.add(Dense(2, activation='softmax'))
+    return model
+
+
 def SA_sst():
     ((x_train_idx_data, y_train_valence, y_train_labels,
-     x_test_idx_data, y_test_valence, y_test_labels,
-     x_valid_idx_data, y_valid_valence, y_valid_labels,
-     x_train_polarity_idx_data, y_train_polarity,
-     x_test_polarity_idx_data, y_test_polarity,
-     x_valid_polarity_idx_data, y_valid_polarity), W) = build_keras_input()
-
+      x_test_idx_data, y_test_valence, y_test_labels,
+      x_valid_idx_data, y_valid_valence, y_valid_labels,
+      x_train_polarity_idx_data, y_train_polarity,
+      x_test_polarity_idx_data, y_test_polarity,
+      x_valid_polarity_idx_data, y_valid_polarity), W) = build_keras_input()
 
     maxlen = 200  # cut texts after this number of words (among top max_features most common words)
     batch_size = 32
-    (X_train, y_train), (X_test, y_test), (X_valid, y_valide) = (x_train_polarity_idx_data, y_train_polarity), (x_test_polarity_idx_data, y_test_polarity), (x_valid_polarity_idx_data, y_valid_polarity)
+    (X_train, y_train), (X_test, y_test), (X_valid, y_valide) = (x_train_polarity_idx_data, y_train_polarity), (
+    x_test_polarity_idx_data, y_test_polarity), (x_valid_polarity_idx_data, y_valid_polarity)
     print(len(X_train), 'train sequences')
     print(len(X_test), 'test sequences')
     # m= 0
@@ -235,7 +276,7 @@ def SA_sst():
     #             if j > m:
     #                 m=j
     # print(m)
-    max_features = W.shape[0] # shape of W: (13631, 300) , changed to 14027 through min_df = 3
+    max_features = W.shape[0]  # shape of W: (13631, 300) , changed to 14027 through min_df = 3
 
     print("Pad sequences (samples x time)")
     X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
@@ -248,7 +289,7 @@ def SA_sst():
     y_test = np_utils.to_categorical(y_test, nb_classes)
     y_valide = np_utils.to_categorical(y_valide, nb_classes)
 
-    model = cnn(W)
+    model = parallel_cnn(W)
     plot(model, to_file='./images/model.png')
 
     # try using different optimizers and different optimizer configs
@@ -257,7 +298,8 @@ def SA_sst():
 
     print("Train...")
     early_stopping = EarlyStopping(monitor='val_loss', patience=10)
-    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=100, validation_data=(X_test, y_test), show_accuracy=True, callbacks=[early_stopping])
+    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=3, validation_data=(X_test, y_test), show_accuracy=True,
+              callbacks=[early_stopping])
     score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
     print('Test score:', score)
     print('Test accuracy:', acc)
@@ -265,8 +307,8 @@ def SA_sst():
 
 def imdb_test():
     # set parameters:
-    max_features = 5000 # number of vocabulary
-    maxlen = 200    # padding
+    max_features = 5000  # number of vocabulary
+    maxlen = 200  # padding
     batch_size = 16
     nb_epoch = 10
 
@@ -292,13 +334,14 @@ def imdb_test():
 
     print("Train...")
     early_stopping = EarlyStopping(monitor='val_loss', patience=10)
-    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=(X_test, y_test), show_accuracy=True, callbacks=[early_stopping])
+    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=(X_test, y_test),
+              show_accuracy=True, callbacks=[early_stopping])
     score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
     print('Test score:', score)
     print('Test accuracy:', acc)
 
 
 if __name__ == "__main__":
-    imdb_test()
-    exit()
+    # imdb_test()
+    # exit()
     SA_sst()
